954a461e222592258355faeb78fe5d1d29495b10
yashodhan10 <yashodhanj10@gmail.com>
2017-01-02 18:15:09 -0500

    Add files via upload
    Added comments

--- load.py
+++ load.py
@@ -6,24 +6,24 @@ import pymongo
 from pymongo import MongoClient
 import sys
 
-g = Graph()
-client = MongoClient()
+g = Graph()    
+client = MongoClient() # establish mongodb connection
 db = client.test
-g.parse("C:\Users\Yashodhanj\Desktop\RDF\gt2rdf.rdf", format= "nt")
+g.parse("gt2rdf.rdf", format= "nt")  # parse the graph as given format; nt in this case
 
 try:
 
 
-	id_count = 1
-#json_data = []
+	id_count = 1 # initialize count
+
 	for s,t,p in g:
 	
-		result = db.pya.find_one({'subject': s})
+		result = db.pya.find_one({'subject': s}) # check if record with the same subject exists
 	#print result
 		if result:
-			db.pya.update({'subject': s}, { '$push': {'relation':{'predicate': t, 'object': p}}})
+			db.pya.update({'subject': s}, { '$push': {'relation':{'predicate': t, 'object': p}}}) # If exists, then add predicate and object in a relation array
 		else:
-				db.pya.insert_one({'_id': id_count, 'subject': s, 'relation': [{'predicate': t, 'object': p}]})
+				db.pya.insert_one({'_id': id_count, 'subject': s, 'relation': [{'predicate': t, 'object': p}]}) # else, create a new document
 				id_count = id_count+1
 
 except:
